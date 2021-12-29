dbb7af7bd96ec47a3db4a82c2ef5aa796dbc7ecb
luisalfredopascualpolanco <61303559+luisalfredopascualpolanco@users.noreply.github.com>
2021-12-28 16:57:19 -0800

    version1.0

---
+++ proyecto10.2/proyecto10.2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto10._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int posicion;
+                int sumatoria = 0;
+                double promedio = 0;
+
+                for (int i = 0; i < 10; i++)
+                {
+                    posicion = i + 1;
+                    Console.Write("Introduzca el valor #" + posicion + ": ");
+                    sumatoria = sumatoria + int.Parse(Console.ReadLine());
+                }
+
+                promedio = sumatoria / 10;
+                Console.WriteLine("La sumatoria de los numeros es "+sumatoria+" y su promedio es "+promedio);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            Console.ReadKey();
+        }
+    }
+}
